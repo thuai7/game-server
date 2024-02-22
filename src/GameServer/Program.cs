bbb3bea0f48128867ba5d46cfa1d5c3fa1e3d14d
Steven C <lemonkyo2222@outlook.com>
2024-02-22 19:42:03 +0800

    CI

--- src/GameServer/Program.cs
+++ src/GameServer/Program.cs
@@ -4,16 +4,13 @@ namespace GameServer;
 
 class Program
 {
-    // https://github.com/THUASTA/EDCHost/blob/main/src/EdcHost/Program.cs
-    const string SerilogTemplate = "[{@t:HH:mm:ss} {@l:u3}] {#if Component is not null}{Component,-13} {#end}{@m}\n{@x}";
-
     static void Main()
     {
         Log.Logger = new LoggerConfiguration()
             .MinimumLevel.Information()
             .WriteTo.Console()
             .CreateLogger();
-        
+
         Version version = typeof(Program).Assembly.GetName().Version ?? new Version(0, 0, 0, 0);
         Log.Information($"THUAI7 GameServer v{version.Major}.{version.Minor}.{version.Build}");
         Log.Information("Copyright (c) ");
