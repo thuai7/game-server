8cb79558e878a5cc24cc76de224c66788e4e97e4
Steven C <lemonkyo2222@outlook.com>
2024-02-22 18:36:43 +0800

    Chore: Create project
    Ignores .sln file.

---
+++ src/GameServer/Program.cs
@@ -0,0 +1,25 @@
+﻿using Serilog;
+
+namespace GameServer;
+
+class Program
+{
+    // https://github.com/THUASTA/EDCHost/blob/main/src/EdcHost/Program.cs
+    const string SerilogTemplate = "[{@t:HH:mm:ss} {@l:u3}] {#if Component is not null}{Component,-13} {#end}{@m}\n{@x}";
+
+    static void Main()
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .WriteTo.Console()
+            .CreateLogger();
+        
+        Version version = typeof(Program).Assembly.GetName().Version ?? new Version(0, 0, 0, 0);
+        Log.Information($"THUAI7 GameServer v{version.Major}.{version.Minor}.{version.Build}");
+        Log.Information("Copyright (c) ");
+
+        // TODO: Create and activate game server
+
+        Task.Delay(-1).Wait();
+    }
+}
