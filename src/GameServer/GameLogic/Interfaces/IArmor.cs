0e447b8127cd2dd7e9fef467068a0a08ac662a93
forgrabbit <2133603132@qq.com>
2024-03-16 10:51:10 +0800

    Interface of Armor and Backpack
    filled the interface code of IArmor and IBackpack

--- src/GameServer/GameLogic/Interfaces/IArmor.cs
+++ src/GameServer/GameLogic/Interfaces/IArmor.cs
@@ -2,5 +2,26 @@ namespace GameServer.GameLogic;
 
 public interface IArmor
 {
-    // TODO: Define the interface
+    public enum Type
+    {
+        NO_ARMOR,
+        PRIMARY_ARMOR,
+        PREMIUM_ARMOR
+    };
+    /// <summary>
+    /// return the type of the armor
+    /// </summary>
+    /// <returns></returns>
+    public Type armorType();
+
+    /// <summary>
+    /// return current points of the armor
+    /// </summary>
+    /// <returns></returns>
+    public int armorPoints();
+
+    /// <summary>
+    /// when the player was injured, call this function to change the points of the armor
+    /// </summary>
+    public void Injured();
 }
--- src/GameServer/GameLogic/Interfaces/IBackpack.cs
+++ src/GameServer/GameLogic/Interfaces/IBackpack.cs
@@ -1,6 +1,43 @@
+using System.Numerics;
+
 namespace GameServer.GameLogic;
 
 public interface IBackPack
 {
-    // TODO: Define the interface
+    /// <summary>
+    /// return the total capacity of the backpack
+    /// </summary>
+    /// <returns></returns>
+    public double totalCapacity();
+
+    /// <summary>
+    /// return the used capacity
+    /// </summary>
+    /// <returns></returns>
+    public double usedCapacity();
+
+    /// <summary>
+    /// return the number of the supply
+    /// </summary>
+    /// <param name="supply"></param>
+    /// <returns></returns>
+    public int supplyNumb(object supply);
+
+
+    /// <summary>
+    /// add supplies, determine whether the capacity is exceeded
+    /// </summary>
+    /// <param name="supply"></param>
+    /// <param name="numb"></param> the number of the suppply
+    /// <returns></returns>
+    public bool addSupplies(object supply, double numb);
+
+
+    /// <summary>
+    /// remove supplies, deter whether the operation succeeds
+    /// </summary>
+    /// <param name="supply"></param>
+    /// <param name="numb"></param>
+    /// <returns></returns>
+    public bool removeSupplies(object supply,double numb);
 }
