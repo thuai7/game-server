9f70c0df8084a5d3a29eecaf6cbae2d15c5bcde7
forgrabbit <2133603132@qq.com>
2024-03-16 12:48:17 +0800

    Update the Map, Barrier, safeZone and add Position
    Update the Map, Barrier, safeZone and add Position

--- src/GameServer/GameLogic/Map.cs
+++ src/GameServer/GameLogic/Map.cs
@@ -1,6 +1,37 @@
+using System.Numerics;
+
 namespace GameServer.GameLogic;
 
 public class Map : IMap
 {
-    // TODO: Implement
+    
+    public Vector<int> mapChunk = new Vector<int>();
+
+    public Dictionary<Position, object> Supplies = new Dictionary<Position, object>();
+
+    public Barrier barrier = new Barrier();
+
+    public SafeZone safeZone = new SafeZone();
+
+    public void GenerateSupplies()
+    {
+        // TODO: Implement
+    }
+
+    public void GenerateMap()
+    {
+        GenerateSupplies();
+        barrier.GenerateBarrier();// TODO: Implement
+    }
+
+    public void updateSupplies()
+    {
+        // TODO: Implement
+    }
+
+    public void updateSafeZone()
+    {
+        int times = 0;  //the safe zone will be ruduced in several times
+        safeZone.SafeZoneUpdate(8*times);// TODO: Implement
+    }
 }
--- src/GameServer/GameLogic/SafeZone.cs
+++ src/GameServer/GameLogic/SafeZone.cs
@@ -1,5 +1,18 @@
+using System.Numerics;
+
 namespace GameServer.GameLogic;
 public class SafeZone : ISafeZone
 {
-    // TODO: Implement
+    
+    public Vector<int> safeChunk = new Vector<int>();
+
+    public bool IsSafe(Position position)
+    {
+        return true;// TODO: Implement
+    }
+
+    public void SafeZoneUpdate(int numb)
+    {
+        // TODO: Implement
+    }
 }
